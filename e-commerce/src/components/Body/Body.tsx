4500583ac34e22ea97f902b31b55f96c6cd16141
Fernando1211 <162237725+Fernando1211@users.noreply.github.com>
2024-10-23 00:27:35 -0300

    update final do visual do site

--- e-commerce/src/components/Body/Body.tsx
+++ e-commerce/src/components/Body/Body.tsx
@@ -1,5 +1,6 @@
 import Menu from './Menu';
 import Produto from './Produto'
+import './Body.css'
 
 const Body = () => {
   return (
@@ -19,31 +20,85 @@ const Body = () => {
 
                
                 
-                <div className="promocoes-container">
+            <div className="promocoes-container">
                 
 
                 <div className="produtos-grid">
                     <Produto
-                        imagem="/path/to/product1.png"
+                        imagem="src/assets/images/cabo.jpg"
                         nome="Cabo Tipo-C/C 2,0m Cb838 Kimaster"
-                        status="Esgotado!"
-                        esgotado={true}
-                    />
-                    <Produto
-                        imagem="/path/to/product2.png"
-                        nome="Teclado Gamer XYZ"
                         status="Disponível"
                         esgotado={false}
                     />
                     <Produto
-                        imagem="/path/to/product3.png"
-                        nome="Mouse Óptico USB"
+                        imagem="src/assets/images/adaptador.png"
+                        nome="Adaptador Hdmi 90º Macho Femea Storm/Oem"
                         status="Disponível"
                         esgotado={false}
                     />
-                </div>
+                    <Produto
+                        imagem="src/assets/images/ssd.png"
+                        nome="Case HD 2.5`` SATA/SSDS USB3.0 BK P GA174"
+                        status="Disponível"
+                        esgotado={false}
+                    />
+                    <Produto
+                        imagem="src/assets/images/luminaria.png"
+                        nome="Luminaria De Emergencia 30 Leds Lea30 Intelbras"
+                        status="Disponível"
+                        esgotado={false}
+                    />
+                    <Produto
+                        imagem="src/assets/images/leitor.png"
+                        nome="Leitor Cartao Memoria 46 Em 1 Ac076 Multilaser"
+                        status="Disponível"
+                        esgotado={false}
+                    />
+                    <Produto
+                        imagem="src/assets/images/headset.png"
+                        nome="Headset H151 Pt 981-000587 Logitech"
+                        status="Disponível"
+                        esgotado={false}
+                    />
+                    <Produto
+                        imagem="src/assets/images/webcam.png"
+                        nome="Webcam C920S Pro Hd 1080p 960-001257 Logitech"
+                        status="Disponível"
+                        esgotado={false}
+                    />
+                    <Produto
+                        imagem="src/assets/images/estabilizador.png"
+                        nome="Estabilizador 500va Biv/115v 60698"
+                        status="Disponível"
+                        esgotado={false}
+                    />
+                </div>              
+
             </div>
+
+            <div className='palavras-container'>
+                <h2 className="palavras-title">AS PALAVRAS</h2>
+                <h2 className="palavras-title-2">PALAVRAS MAIS BUSCADAS</h2>
+            </div>
+
         </div>
+
+        <div className='body-lower-container'>
+            <div className='lower-text-container'>
+                <h2>GOSTOU DA LOJA?</h2>
+                <h3>ENTÃO CADASTRE-SE!</h3>
+            </div>
+            <div className='lower-containet-icon'></div>
+            <div className='lower-text-container'><h2>NEWSLETTER</h2></div>            
+            <input 
+                type="text" 
+                minLength={2}
+                placeholder="Seu e-mail" 
+                className="barra-cadastro" 
+            />
+            <button className='cadastro-button'>CADASTRAR</button>
+        </div>
+
     </body>
 
   );
--- e-commerce/src/components/Body/Produto.tsx
+++ e-commerce/src/components/Body/Produto.tsx
@@ -11,12 +11,17 @@ interface ProdutoProps {
 const Produto: React.FC<ProdutoProps> = ({ imagem, nome, status, esgotado }) => {
     return (
         <div className="produto-container">
-            <img src={imagem} alt={nome} className="produto-image" />
-            <h3>{nome}</h3>
-            <span className={esgotado ? 'status esgotado' : 'status'}>{status}</span>
-            <button className="comprar-button" disabled={esgotado}>
-                {esgotado ? 'ESGOTADO' : 'COMPRAR'}
-            </button>
+            <div className='produto-image-container'>
+                <img src={imagem} alt={nome} className="produto-image" />
+                <div className='produto-hover-overlay'>
+                    <span>DETALHES</span>
+                </div>
+                <h2>{nome}</h2>
+                <span className={esgotado ? 'status esgotado' : 'status'}>{status}</span>
+                <button className="comprar-button" disabled={esgotado}>
+                    {esgotado ? 'ESGOTADO' : 'COMPRAR'}
+                </button>
+            </div>            
         </div>
     );
 };
